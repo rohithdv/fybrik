ab81c2d13db72d97fc7931f1b0f8eaa73efaa6c2
rohithdv <rovallam@in.ibm.com>
2021-06-25 17:26:38 +0530

    adding manager mock client
    Signed-off-by: rohithdv <rovallam@in.ibm.com>

--- pkg/connectors/manager-client2.go
+++ pkg/connectors/manager-client2.go
@@ -14,7 +14,7 @@ func main() {
 	//mainPolicyManagerName := os.Getenv("MAIN_POLICY_MANAGER_NAME")
 	mainPolicyManagerName := "OPEN API MANAGER"
 	//mainPolicyManagerURL := os.Getenv("MAIN_POLICY_MANAGER_CONNECTOR_URL")
-	mainPolicyManagerURL := "http://localhost:8080"
+	mainPolicyManagerURL := "http://v2-opa-connector:8080"
 	//connectionTimeout, err := getConnectionTimeout()
 	timeOutInSeconds := 120
 	connectionTimeout := time.Duration(timeOutInSeconds) * time.Second
@@ -24,7 +24,9 @@ func main() {
 		return
 	}
 
-	input := []openapiclient.PolicymanagerRequest{*openapiclient.NewPolicymanagerRequest(*openapiclient.NewAction(openapiclient.ActionType("read")), *openapiclient.NewResource("Name_example"))} // []PolicymanagerRequest | input values that need to be considered for filter
+	input := []openapiclient.PolicymanagerRequest{*openapiclient.NewPolicymanagerRequest(*openapiclient.NewAction(openapiclient.ActionType("read")), *openapiclient.NewResource("{\"asset_id\": \"0bb3245e-e3ef-40b7-b639-c471bae4966c\", \"catalog_id\": \"503d683f-1d43-4257-a1a3-0ddf5e446ba5\"}"))} // []PolicymanagerRequest | input values that need to be considered for filter
+	creds := "http://vault.m4d-system:8200/v1/kubernetes-secrets/wkc-creds?namespace=cp4d"
+	input[0].Resource.Creds = &creds
 	response, err := policyManager.GetPoliciesDecisions(&input[0])
 
 	fmt.Fprintf(os.Stdout, "Response from `DefaultApi.GetPoliciesDecisions`: %v\n", response)
