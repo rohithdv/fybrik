0ef7c56af555cad20eb225bffe9a6c8256586739
Rohith <rohith@datameshproj.sl.cloud9.ibm.com>
2021-06-25 12:35:56 +0000

    updated deployment files for manager-client
    Signed-off-by: Rohith <rohith@datameshproj.sl.cloud9.ibm.com>

---
+++ pkg/connectors/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Copyright 2020 IBM Corp.
+# SPDX-License-Identifier: Apache-2.0
+
+set -x
+set -e
+
+: ${KUBE_NAMESPACE:=m4d-system}
+: ${WITHOUT_VAULT=true}
+: ${ROOT_DIR=../..}
+
+manager_client_delete() {
+        printf "\nRemoving kubectl resources on active cluster"
+        $ROOT_DIR/hack/tools/bin/kustomize build . | kubectl delete -f - || true
+}
+
+manager_client_create() {
+        $ROOT_DIR/hack/tools/bin/kustomize build . | kubectl apply -f -
+}
+
+config() {
+        kubectl create secret docker-registry cloud-registry  \
+                --docker-server="$DOCKER_HOSTNAME" \
+                --docker-username="$DOCKER_USERNAME" \
+                --docker-password="$DOCKER_PASSWORD" \
+                -n $KUBE_NAMESPACE || true
+        kubectl patch serviceaccount default -p \
+                "{\"imagePullSecrets\": [{\"name\": \"cloud-registry\"}]}" \
+                -n $KUBE_NAMESPACE || true
+}
+
+undeploy() {
+        manager_client_delete
+}
+
+deploy() {
+	config
+        manager_client_create
+}
+
+case "$1" in
+undeploy)
+    undeploy
+    ;;
+*)
+    deploy
+    ;;
+esac
