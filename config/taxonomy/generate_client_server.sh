c6e1d8a5745fc1782c53e27520cb3e385dc003fa
Rohith D Vallam <rovallam@in.ibm.com>
2021-07-06 12:41:31 +0000

    minor changes
    Signed-off-by: Rohith D Vallam <rovallam@in.ibm.com>

--- config/taxonomy/generate_client_server.sh
+++ config/taxonomy/generate_client_server.sh
@@ -1,9 +1,10 @@
-# the below is used as-is. 
-openapi-generator-cli generate -g go --additional-properties=prependFormOrBodyParameters=true,packageName=openapi  --global-property=models -o ../../pkg/connectors/taxonomy_models_codegen -i test4.yaml
+# Code generated due to the below command is used as-is WITHOUT any modification
+openapi-generator-cli generate -g go --additional-properties=prependFormOrBodyParameters=true,packageName=openapi  --global-property=models -o ../../pkg/connectors/taxonomy_models_codegen -i m4d-policy-manager-taxonomy.yaml
 
-# the below is also modified to suit for our purposes 
+# Code generated due to the below 2 commands is MODIFIED to suit our purposes
+
 # below code is the based for implementing the http client 
-# openapi-generator-cli generate -g go --additional-properties=prependFormOrBodyParameters=true,packageName=openapi  --global-property=apis,supportingFiles -o ../../pkg/connectors/taxonomy_client_apis_codegen2 -i test4.yaml
+# openapi-generator-cli generate -g go --additional-properties=prependFormOrBodyParameters=true,packageName=openapi  --global-property=apis,supportingFiles -o ../../pkg/connectors/taxonomy_client_apis_codegen2 -i m4d-policy-manager-taxonomy.yaml
 
 # below code is the based for implementing the http server 
-# openapi-generator-cli generate -g go-server  --additional-properties=packageName=openapiserver,serverPort=8081,sourceFolder=openapiserver  --global-property=apis,supportingFiles -o out_go_server -i test4.yaml
+# openapi-generator-cli generate -g go-server  --additional-properties=packageName=openapiserver,serverPort=8081,sourceFolder=openapiserver  --global-property=apis,supportingFiles -o out_go_server -i m4d-policy-manager-taxonomy.yaml
